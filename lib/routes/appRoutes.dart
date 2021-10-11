6bfd7734734e790579c77cc0e95b316325c4b696
Andrey Lapitski <lapitski.dev@gmail.com>
2021-10-11 10:56:02 +0400

    routes

---
+++ lib/routes/appRoutes.dart
@@ -0,0 +1,18 @@
+import 'package:humidor_one_by_favre/screens/homescreen/homeScreen.dart';
+import 'package:humidor_one_by_favre/screens/settings/settingsScreen.dart';
+
+class AppRoutes {
+  static const HOME_SCREEN = 'homescreen';
+  static const SETTINGS_SCREEN = 'settings';
+
+  static initialRoute() {
+    return HOME_SCREEN;
+  }
+
+  static getRoutes() {
+    return {
+      HOME_SCREEN: (ctx) => HomeScreen(),
+      SETTINGS_SCREEN: (ctx) => SettingsScreen(),
+    };
+  }
+}
