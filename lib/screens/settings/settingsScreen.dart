78ad9e0fac3e9e69ded8cb5178dfc7425e1388d0
Andrey Lapitski <lapitski.dev@gmail.com>
2021-10-11 10:56:19 +0400

    screens

---
+++ lib/screens/settings/settingsScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Settings'),
+        ),
+        body: Container());
+  }
+}
