ee9ea0b8807ff4455e428ac7c4d9a1e80816cc39
lapitski <7media73@gmail.com>
2021-10-13 14:10:06 +0400

    customWrapper

--- lib/common/commonWidgets.dart
+++ lib/common/commonWidgets.dart
@@ -7,3 +7,19 @@ class CustomProgressIndicator extends StatelessWidget {
     );
   }
 }
+
+class CustomWrapper extends StatelessWidget {
+  final Widget child;
+
+  CustomWrapper({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: child,
+      ),
+    );
+  }
+}
