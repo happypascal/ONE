f0a79d246d66579454b8fec0c84033bd0e715e77
lapitski <7media73@gmail.com>
2021-10-14 12:53:26 +0400

    coils/ Hr addresses

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,3 +1,16 @@
 class Const {
   static const DEFAULT_ADDRESS = '192.168.0.129';
+
+  //Coils
+  static const int OPEN_CLOSE = 134;
+  static const int HUM_MINUS = 9;
+  static const int HUM_PLUS = 10;
+  static const int TEMP_MINUS = 36;
+  static const int TEMP_PLUS = 35;
+
+  //Holding Registers
+  static const int HUM_MEASURE = 21;
+  static const int HUM_SET = 5;
+  static const int TEMP_MEASURE = 51;
+  static const int TEMP_SET = 53;
 }
