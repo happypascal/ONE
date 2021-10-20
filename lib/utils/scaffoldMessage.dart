062dac3277f5c8a3ae7912187d1b942f35a35f4d
lapitski <7media73@gmail.com>
2021-10-20 13:29:13 +0400

    scaffold message update

--- lib/utils/scaffoldMessage.dart
+++ lib/utils/scaffoldMessage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:humidor_one_by_favre/routes/appRoutes.dart';
 
 class ScaffoldMessage {
   static showErrorMessage(BuildContext context, String msg) {
@@ -6,6 +7,20 @@ class ScaffoldMessage {
       SnackBar(
         content: Text(msg),
         backgroundColor: Colors.red,
+        duration: Duration(seconds: 5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20.0),
+          ),
+        ),
+        action: SnackBarAction(
+          label: 'Settings',
+          textColor: Colors.white,
+          onPressed: () {
+            Navigator.of(context)
+                .pushReplacementNamed(AppRoutes.SETTINGS_SCREEN);
+          },
+        ),
       ),
     );
   }
